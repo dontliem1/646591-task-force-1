2c0ca859a99b4d12ac921741e2cfa903def6bc82
Михаил Воробьев <dontliem1@gmail.com>
2020-05-03 02:09:21 +0500

    Added models via Gii. Created Tasks page.

---
+++ frontend/models/User.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $email Email
+ * @property string $name Name and surname
+ * @property int $city_id Chosen city
+ * @property string $password Password hash
+ * @property string $dt_add Date registered
+ * @property string|null $last_activity_time Time of last activity
+ *
+ * @property Messages[] $messages
+ * @property Messages[] $messages0
+ * @property Notifications[] $notifications
+ * @property Opinions[] $opinions
+ * @property Opinions[] $opinions0
+ * @property Profiles $profiles
+ * @property Replies[] $replies
+ * @property Tasks[] $tasks
+ * @property Cities $city
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['email', 'name', 'city_id', 'password', 'dt_add'], 'required'],
+            [['city_id'], 'integer'],
+            [['dt_add', 'last_activity_time'], 'safe'],
+            [['email', 'name', 'password'], 'string', 'max' => 255],
+            [['email'], 'unique'],
+            [['city_id'], 'exist', 'skipOnError' => true, 'targetClass' => City::className(), 'targetAttribute' => ['city_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'Email',
+            'name' => 'Name',
+            'city_id' => 'City ID',
+            'password' => 'Password',
+            'dt_add' => 'Dt Add',
+            'last_activity_time' => 'Last Activity Time',
+        ];
+    }
+
+    /**
+     * Gets query for [[Messages]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMessages()
+    {
+        return $this->hasMany(Message::className(), ['sender_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Messages0]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMessages0()
+    {
+        return $this->hasMany(Message::className(), ['recipient_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Notifications]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getNotifications()
+    {
+        return $this->hasMany(Notification::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Opinions]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOpinions()
+    {
+        return $this->hasMany(Opinion::className(), ['customer_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Opinions0]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOpinions0()
+    {
+        return $this->hasMany(Opinion::className(), ['executor_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Profiles]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProfiles()
+    {
+        return $this->hasOne(Profile::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Replies]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReplies()
+    {
+        return $this->hasMany(Reply::className(), ['executor_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Tasks]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTasks()
+    {
+        return $this->hasMany(Task::className(), ['customer_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[City]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCity()
+    {
+        return $this->hasOne(City::className(), ['id' => 'city_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return UserQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new UserQuery(get_called_class());
+    }
+}
